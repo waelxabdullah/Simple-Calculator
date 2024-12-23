5f96ff228f5260fa39ee385bbf6a8155a114dfef
Wael <411109515@qu.edu.sa>
2024-12-23 12:56:06 +0300

    Done easy

---
+++ Simple Calculator.py
@@ -0,0 +1,7 @@
+print("Enter Two Number to Perform basic arithmetic operations (addition, subtraction, multiplication, division).")
+n1= int(input("First Number= "))
+n2= int(input("Secend Number= "))
+print(f"addition of the two number {n1} + {n2} = {n1+n2}")
+print(f"subtraction of the two number {n1} - {n2} = {n1-n2}")
+print(f"multiplication of the two number {n1} x {n2} = {n1*n2}")
+print(f"division of the two number {n1} / {n2} = {n1/n2}")
